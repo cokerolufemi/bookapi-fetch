349ebcc74e3ca3a622f46e6a1b051de2a1a8a067
coker <goocng@gmail.com>
2023-01-31 00:18:14 -0800

    bookapi-fetch

---
+++ bookstore-backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://cokerolufemigbolahan:gbossey1234@cluster0.lqes6k1.mongodb.net/express_db?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
